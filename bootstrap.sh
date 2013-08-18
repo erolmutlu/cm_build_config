f383683eeef25529aa067f3149b9e542d62a6bde
David Rodero <david@codiguiel.net>
2013-08-18 18:33:29 +0200

    Disable clean builds

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 export PATH="$PATH:/opt/local/bin/:/usr/local/bin/"
 
-export CLEAN=true
+export CLEAN=false
 export LUNCH=cm_galaxysl-userdebug
 export REPO_BRANCH=cm-10.2
 export RELEASE_TYPE=CM_NIGHTLY
