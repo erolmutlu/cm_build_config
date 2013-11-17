7764a914953e3377c606cf74d9501de98490b78a
David Rodero <david@codiguiel.net>
2013-11-17 21:51:09 +0100

    Updated manifests to cm-11.0 and phablet-saucy.
    Signed-off-by: David Rodero <david@codiguiel.net>

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,5 @@ export PATH="$PATH:/opt/local/bin/:/usr/local/bin/"
 
 export CLEAN=true
 export LUNCH=cm_galaxysl-userdebug
-export REPO_BRANCH=cm-10.2
+export REPO_BRANCH=cm-11.0
 export RELEASE_TYPE=CM_NIGHTLY
