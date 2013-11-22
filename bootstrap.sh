380942963a67fa6cbed82b3ea7e142e43c51c7f0
erolmutlu <mutlu@erolmutlu.com>
2013-11-22 20:27:34 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 export PATH="$PATH:/opt/local/bin/:/usr/local/bin/"
 
 export CLEAN=false
-export LUNCH=cm_galaxysl-userdebug
+export LUNCH=cm_i9100-userdebug
 export REPO_BRANCH=cm-11.0
 export RELEASE_TYPE=CM_NIGHTLY
